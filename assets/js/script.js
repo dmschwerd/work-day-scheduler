98507afffd893fc70ffc7d76e8c986ae1ceba6d9
David <dmsch656@gmail.com>
2023-04-05 15:57:35 -0700

    color coded time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var moment = moment();
 
 // the current day is displayed at the top of the calendar
 var currentDate = function() {
-    $("#currentDay").text(moment.format("dddd[,] MMMM Do[,] YYYY"));
+    $("#current-day").text(moment.format("dddd[,] MMMM Do[,] YYYY"));
 };
 
 // time blocks for standard business hours
@@ -31,16 +31,17 @@ var createTimeblocks = function(hourStart, hourEnd) {
                 .addClass("hour")
                 .text(sum + ":00 p.m.");
         } else {
-            sum = sum - 12;
             var hour = $("<div>")
                 .addClass("hour")
-                .text(sum + ":00 p.m.");
+                .text(sum - 12 + ":00 p.m.");
         }
 
         $(".new-row").append(hour);
 
         var events = $("<div>")
             .addClass("past");
+
+        auditTask(events, sum);
         
         $(".new-row").append(events);
 
@@ -66,22 +67,33 @@ var saveTasks = function() {
 }
 */
 
-/* each time block is color-coded to indicate whether it is in the past, present, or future
-var auditTask = function() {
+// each time block is color-coded to indicate whether it is in the past, present, or future
+var auditTask = function(taskEl, hour) {
+    // compare hour to current time
+    console.log(moment.toString());
+    var currentHour = moment.format("ddd MMM DD YYYY[ ]" + hour + "[:00:00 GMT-0700]");
+    var roundedHour = moment.startOf("hour");
+
+    if(roundedHour.isSame(currentHour)) {
+        taskEl.addClass("present");
+    } else if(roundedHour.isBefore(currentHour)) {
+        taskEl.addClass("future");
+    }
 
 };
-
-setInterVal(function() {
-
-}, 1000 * 60);
-
-// the current day is displayed at the top of the calendar
-setInterVal(function() {
+/*
+setInterval(function() {
 
 }, 1000 * 60);
 */
 
-
+/* the current day is displayed at the top of the calendar
+setInterval(function() {
+    $(".currentDay").each(function(index, el) {
+        auditTask(el);
+    });
+}, 1000 * 60);
+*/
 var run = function() {
     currentDate();
     createTimeblocks("9", "17");
